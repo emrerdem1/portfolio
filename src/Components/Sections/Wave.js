45af95997d79789b5b57f48529f0d6bf74dfcbc3
emrerdem1 <emrerdem94@gmail.com>
2020-07-02 01:36:56 +0300

    add checkpoint

--- src/Components/Sections/Wave.js
+++ src/Components/Sections/Wave.js
@@ -22,6 +22,21 @@ export const Wave = () => {
         preserveAspectRatio="none"
         className="triple-wave"
       >
+        <defs>
+          <linearGradient
+            id="gradient"
+            x1="0%"
+            y1="0%"
+            x2="50%"
+            y2="0%"
+            x3="100%"
+            y3="0%"
+          >
+            <stop offset="0%" stop-color="#24243e" />
+            <stop offset="50%" stop-color="#0f0c29" />
+            <stop offset="100%" stop-color="#302b63" />
+          </linearGradient>
+        </defs>
         <path
           d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
           opacity=".25"
