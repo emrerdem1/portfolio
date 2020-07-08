cb67b24cf575564185a8777bd3d820567c84b312
emrerdem1 <emrerdem94@gmail.com>
2020-07-08 21:36:51 +0300

    set project details

--- src/Components/Sections/AboutComponents/ProjectOpen.jsx
+++ src/Components/Sections/AboutComponents/ProjectOpen.jsx
@@ -8,8 +8,13 @@ export const ProjectOpen = () => {
         <h3 className="col-12">MadLibs</h3>
         <div className="project-open__single--madlibs">
           <div className="project-open__single--madlibs-svg">
-            <span className="svg-description"></span>
-            <span className="svg-tools"></span>
+            <span className="svg-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              sed consectetur sit praesentium explicabo ad ab iste numquam eius
+              maxime sequi voluptas, dignissimos omnis suscipit accusamus
+              perspiciatis ducimus adipisci maiores.
+            </span>
+            <span className="svg-tools">HTML, CSS, Vanilla JS</span>
           </div>
           <div className="project-open__single--madlibs-anchor">
             <a href="#" title="Demo"></a>
@@ -22,8 +27,15 @@ export const ProjectOpen = () => {
         <h3 className="col-12">Movie</h3>
         <div className="project-open__single--movie">
           <div className="project-open__single--movie-svg">
-            <span className="svg-description"></span>
-            <span className="svg-tools"></span>
+            <span className="svg-description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
+              reiciendis eaque rem. Repellat quas aliquam error fugit a
+              recusandae beatae libero, id, et ad excepturi? Autem quia aut
+              minus eos!
+            </span>
+            <span className="svg-tools">
+              HTML, CSS, Bootstrap, Postman, Vanilla JS
+            </span>
           </div>
           <div className="project-open__single--movie-anchor">
             <a href="#" title="Demo"></a>
@@ -36,8 +48,15 @@ export const ProjectOpen = () => {
         <h3 className="col-12">Board</h3>
         <div className="project-open__single--board">
           <div className="project-open__single--board-svg">
-            <span className="svg-description"></span>
-            <span className="svg-tools"></span>
+            <span className="svg-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nemo
+              officia magnam dolor tenetur rerum earum repellendus hic repellat
+              fugiat nihil, corrupti quasi aut id praesentium reprehenderit rem
+              architecto laborum.
+            </span>
+            <span className="svg-tools">
+              HTML, CSS, Bootstrap, Material UI, ReactJS, Firebase
+            </span>
           </div>
           <div className="project-open__single--board-anchor">
             <a href="#" title="Demo"></a>
