4bf51dc310ed8fc2ec4c2b262f7ee328568ca8a4
emrerdem1 <emrerdem94@gmail.com>
2020-09-10 14:18:09 +0300

    Fix: a typo

--- src/Components/Sections/about/Project.constants.js
+++ src/Components/Sections/about/Project.constants.js
@@ -2,7 +2,7 @@ const MADLIBS_DESCRIPTION = `Write out a full story that is processed into a for
             allow the one to keep track of "blanks". Whenever the user updates a
             blank in the edit view, it should update the preview any time a new
             character is typed.`;
-const MADLIBS_TOOLS = `HTML, CSS, Vanilla JS`;
+const MADLIBS_TOOLS = `Vanilla JS, HTML, CSS, Git`;
 const MADLIBS_LINKS = {
   repo: "https://github.com/emrerdem1/MadLibs-Project",
   demo: "https://madlibs-project.netlify.app/",
@@ -12,7 +12,7 @@ const BOARD_DESCRIPTION = `Build a ToDo Board that users can schedule their task
             create custom boards with unlimited amount of tasks within, move the
             position of boards freely, change tasks status by clicking and
             sorting boards to one's heart's content.`;
-const BOARD_TOOLS = `HTML, CSS, React-Bootstrap, Material UI, ReactJS, Firebase`;
+const BOARD_TOOLS = `ReactJS, Firebase, React-Bootstrap, Material UI, HTML, CSS, Git`;
 const BOARD_LINKS = {
   repo: "https://github.com/emrerdem1/ToDo-BoardProject",
   demo: "https://todo-board-project.netlify.app/",
@@ -22,15 +22,15 @@ const MOVIE_DESCRIPTION = `Do the styling of Single Movie Page that is fetched f
             DB API and build functionalities to move from Movie Page to Actor
             Page with necessary fields getting filled. Do the search and sorting
             based on names, get recommendation and trailer section by using API.`;
-const MOVIE_TOOLS = `HTML, CSS, Bootstrap, Postman, Vanilla JS`;
+const MOVIE_TOOLS = `Vanilla JS, Bootstrap, Postman, HTML, CSS, Git`;
 const MOVIE_LINKS = {
   repo: "https://github.com/emrerdem1/movie-website_w-API",
   demo: "https://themoviedb-api.netlify.app/",
 };
 
 const PORTFOLIO_DESCRIPTION = `My playground that contains where I'm from and what my passions are as well as projects
-from the little scaled ones I've worked on Front-End Developer Bootcamps to the ones I'm gonna develop that I'm proud of.`;
-const PORTFOLIO_TOOLS = `HTML, SCSS, Bootstrap, ReactJS`;
+from the little scaled ones I've worked on Front-End Developer Bootcamp to the ones I'm gonna develop that I'm proud of.`;
+const PORTFOLIO_TOOLS = `ReactJS, Bootstrap, HTML, SCSS, Git`;
 const PORTFOLIO_LINKS = {
   repo: "https://github.com/emrerdem1/portfolio",
   demo: "https://emrerdem.com/",
@@ -39,7 +39,7 @@ const PORTFOLIO_LINKS = {
 const ECOMMERCE_DESCRIPTION = `E-commerce website where users can sign-up, have access to their own profile pages,
 do live searching, add items to shopping cart, read educational news and articles brought forth by Wordpress API.
 The website is supported by dual language and aim to provide health related products and articles.`;
-const ECOMMERCE_TOOLS = `HTML, SCSS, React-Bootstrap, ReactJS, Redux, i18next, Firebase, Wordpress API`;
+const ECOMMERCE_TOOLS = `ReactJS, Redux, Firebase, Wordpress API, i18next, React-Bootstrap, HTML, SCSS, Git`;
 const ECOMMERCE_LINKS = {
   repo: "https://github.com/emrerdem1/istanbul-capstone-ecommerce",
   demo: "https://e-commerce-capstone.netlify.app/",
@@ -80,5 +80,5 @@ export const PROJECTS = [
     tools: MADLIBS_TOOLS,
     demoLink: MADLIBS_LINKS.demo,
     repoLink: MADLIBS_LINKS.repo,
-  }
+  },
 ];
