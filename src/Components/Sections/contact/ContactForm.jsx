5ca3ed5cc87f77454092f4f3dc1dcff83a3ba3ac
emrerdem1 <emrerdem94@gmail.com>
2020-12-08 17:14:05 +0300

    Edit: add recaptcha, implement antd form

--- src/Components/Sections/contact/ContactForm.jsx
+++ src/Components/Sections/contact/ContactForm.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Form, Input, Button } from "antd";
 import emailjs from "emailjs-com";
 import { init } from "emailjs-com";
-import { SERVICE_ID, TEMPLATE_ID, USER_ID } from "./EmailCredit";
+import { SERVICE_ID, TEMPLATE_ID, USER_ID, RECAPTCHA_KEY } from "./EmailCredit";
+import Reaptcha from "reaptcha";
 
 export const ContactForm = () => {
-  useEffect(() => {
-    init(USER_ID);
-  }, []);
-
+  const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
+  const [isRecaptchaLoaded, setIsRecaptchaLoaded] = useState(false);
   const [inputValue, setInput] = useState({
     user_name: null,
     user_mail: null,
@@ -16,6 +16,12 @@ export const ContactForm = () => {
     user_message: null,
   });
 
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    init(USER_ID);
+  }, []);
+
   const handleForm = ({ target }) =>
     setInput((prevState) => {
       return { ...prevState, [target.name]: target.value };
@@ -23,62 +29,126 @@ export const ContactForm = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, inputValue, USER_ID).then(
-      (response) => {
-        console.log("SUCCESS!", response.status, response.text);
-      },
-      (err) => {
-        console.log("FAILED...", err);
-      }
-    );
+    if (isRecaptchaVerified) {
+      console.log(
+        "verified,",
+        SERVICE_ID,
+        TEMPLATE_ID,
+        inputValue,
+        USER_ID,
+        emailjs
+      );
+      /*emailjs.send(SERVICE_ID, TEMPLATE_ID, inputValue, USER_ID).then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );*/
+    } else {
+      console.log("not verified");
+    }
+  };
+
+  const onVerify = (recaptchaResponse) => {
+    if (!recaptchaResponse) {
+      return;
+    }
+
+    setIsRecaptchaVerified(true);
+  };
+
+  const handleSubmit = (fieldValues) => {
+    console.log(fieldValues);
   };
 
   return (
-    <div className="row contact-form__wrapper">
-      <div className="col-12 col-lg-4 col-md-12 col-sm-12 contact-svg">
-        <h2 className="contact-svg__title">Contact me!</h2>
-        <p className="contact-svg__text">
-          I love making acquaintance with new people!
-        </p>
-        <svg className="contact-svg__img"></svg>
+    <>
+      <div className="row contact-form__wrapper">
+        <div className="col-12 col-lg-4 col-md-12 col-sm-12 contact-svg">
+          <h2 className="contact-svg__title">Contact me!</h2>
+          <p className="contact-svg__text">
+            I love making acquaintance with new people!
+          </p>
+          <svg className="contact-svg__img"></svg>
+        </div>
+        <div className="col-12 col-lg-6 col-md-12 col-sm-12 form-container">
+          <form onSubmit={sendEmail}>
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Your name"
+              onChange={handleForm}
+            />
+            <input
+              type="text"
+              name="user_mail"
+              placeholder="Your e-mail"
+              onChange={handleForm}
+            />
+            <input
+              type="text"
+              name="user_gsm"
+              placeholder="Your contact number"
+              onChange={handleForm}
+            />
+            <input
+              type="text"
+              placeholder="Subject..."
+              name="user_subject"
+              onChange={handleForm}
+            />
+            <textarea
+              type="text"
+              placeholder="Kindly let me know how can I help you."
+              name="user_message"
+              onChange={handleForm}
+            ></textarea>
+            <button
+              type="submit"
+              className="contact-button"
+              disabled={!isRecaptchaLoaded}
+            >
+              Send
+            </button>
+
+            <Reaptcha
+              theme="dark"
+              sitekey={RECAPTCHA_KEY}
+              onLoad={() => setIsRecaptchaLoaded(true)}
+              onVerify={onVerify}
+              className="formRecaptcha"
+            />
+          </form>
+        </div>
       </div>
-      <div className="col-12 col-lg-6 col-md-12 col-sm-12 form-container">
-        <form onSubmit={sendEmail}>
-          <input
-            type="text"
-            name="user_name"
-            placeholder="Your name"
-            onChange={handleForm}
-          />
-          <input
-            type="text"
-            name="user_mail"
-            placeholder="Your e-mail"
-            onChange={handleForm}
-          />
-          <input
-            type="text"
-            name="user_gsm"
-            placeholder="Your contact number"
-            onChange={handleForm}
-          />
-          <input
-            type="text"
-            placeholder="Subject..."
-            name="user_subject"
-            onChange={handleForm}
-          />
-          <textarea
-            type="text"
-            placeholder="Kindly let me know how can I help you."
-            name="user_message"
-            onChange={handleForm}
-          ></textarea>
-          <button type="submit" className="contact-button">
-            Send
-          </button>
-        </form>
+      <div className="row contact-form__wrapper">
+        <div className="col-12 col-lg-6 col-md-12 col-sm-12 form-container">
+          <Form layout="vertical" form={form} onFinish={handleSubmit}>
+            <Form.Item label="Name">
+              <Input placeholder="Your name" />
+            </Form.Item>
+            <Form.Item label="E-mail">
+              <Input placeholder="Your e-mail" />
+            </Form.Item>
+            <Form.Item label="Contact number">
+              <Input placeholder="Your contact number" />
+            </Form.Item>
+            <Form.Item label="Subject">
+              <Input placeholder="Subject" />
+            </Form.Item>
+            <Form.Item label="Message">
+              <Input.TextArea placeholder="Kindly let me know how can I help you." />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" className="contact-button">
+                Send
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/Components/Sections/contact/EmailCredit.jsx
+++ src/Components/Sections/contact/EmailCredit.jsx
@@ -1,3 +1,4 @@
 export const TEMPLATE_ID = "template_mo46eem";
 export const SERVICE_ID = "service_97w1o0a";
 export const USER_ID = "user_L04fXu9pDZ5K9668PFzC1";
+export const RECAPTCHA_KEY = "6Lfmav0ZAAAAAMj3IuLJFlusx78XRmW4SWz2q7-V";
