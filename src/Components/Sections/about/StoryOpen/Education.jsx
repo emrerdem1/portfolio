ed17835650d6917d4a1d741f75cb3d62c5699c0c
emre erdem <emrerdem94@gmail.com>
2021-05-22 17:18:21 +0300

    Update resume link

--- src/Components/Sections/about/StoryOpen/Education.jsx
+++ src/Components/Sections/about/StoryOpen/Education.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const RESUME_GDRIVE =
-  "https://drive.google.com/file/d/1XJtv2TdBuCnwXA7IUpwt2CSqRIncnU2Z/view?usp=sharing";
+import React from 'react';
+import { RESUME } from '../Project.constants';
 
 export const Education = () => {
   return (
@@ -11,29 +9,29 @@ export const Education = () => {
         <h2>Education</h2>
         <p>
           <i>
-            (You can download{" "}
+            (You can download{' '}
             <span>
               <a
                 className="outer-links"
-                href={RESUME_GDRIVE}
+                href={RESUME}
                 title="CV"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Resume
               </a>
-            </span>{" "}
+            </span>{' '}
             for the summary.)
           </i>
           I finished my elementary, junior and highschool education in my
-          hometown, moving to a different state to get my Bachelor's Degree in{" "}
+          hometown, moving to a different state to get my Bachelor's Degree in{' '}
           <span>Management Information Systems</span> department where I have
           been got to learn a variety of programming languages and databases
           such as <span>HTML, CSS, C#, PHP</span> and <span>Oracle</span>!
         </p>
         <p>
           After my graduation in MIS, I am blessed to participate in a Front-End
-          Web Developer Bootcamp in{" "}
+          Web Developer Bootcamp in{' '}
           <a
             className="outer-links"
             href="https://www.re-coded.com/"
@@ -43,13 +41,13 @@ export const Education = () => {
             Re:Coded
           </a>
           ! I got to learn many new approaches and get accustomed to web
-          developer tools such as{" "}
+          developer tools such as{' '}
           <span>
             React, TypeScript, GraphQL, Redux, Firebase, Vanilla JS by building
             real web applications and more
-          </span>{" "}
+          </span>{' '}
           with the help of caring instructors Re:Coded Team assigned to us and
-          curriculum of{" "}
+          curriculum of{' '}
           <a
             className="outer-links"
             href="https://flatironschool.com/"
